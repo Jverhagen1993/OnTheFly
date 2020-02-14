650449e9cb018cc3589c8ce9584c55faa73c6228
Jurgen Verhagen <d253670@edu.rocwb.nl>
2020-02-14 11:58:37 +0100

    test

--- Oefenopdrachten/opdracht1.php
+++ Oefenopdrachten/opdracht1.php
@@ -1,8 +1,19 @@
+<?php
+include ("config.php");
+
+$query = "SELECT * FROM smaken"
+$stm = $conn->prepare($query);
+if($stm->execute())
+	{
+		$res = $stm->fetchAll(PDO::FETCH_OBJ);
+		foreach($res as $smaak)
+		{
+			echo "<a href='newpage.php?smid=".$smaak->smid."'>".$smaak->smaak;"</a>
+		}
+	}
+
+?>
 <form>
 	<input type="Text" name="test"/>
 	<input type="submit" name="button"/>
 </form>
-<?php
-
-
-?>
